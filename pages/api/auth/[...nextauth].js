0891a0057bebbabd8a954295b89675f9b4c7dd3d
felixhoentzsch <felix.hoentzsch@icloud.com>
2023-09-17 19:28:08 +0200

    Änderungen

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,6 +21,7 @@ export default NextAuth({
         try {
           await mongodb.dbConnect();
           const user = await UserModel.findOne({ username });
+          console.log(user)
 
           if (!user) {
             return null;
@@ -35,6 +36,14 @@ export default NextAuth({
             return null;
           }
 
+          const userData= {
+            id: user._id,
+            username: user.username,
+            role: user.role
+          }
+
+          console.log(userData)
+
           return user;
         } catch (error) {
           console.log("Error: ", error);
@@ -43,10 +52,66 @@ export default NextAuth({
     }),
   ],
   session: {
+    storage:"sessionStorage",
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/create"
   },
+  cookie: {
+    secure: false,
+  },
+  storage: "sessionStorage",
+
 });
+
+
+// import NextAuth from "next-auth/next";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import bcrypt from "bcryptjs";
+// import mongodb from "@/utils/mongodb";
+// import UserModel from "@/models/user";
+
+// const authOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       name: "credentials",
+//       credentials: {},
+
+//       async authorize(credentials) {
+//         const { username, passwort } = credentials;
+
+//         try {
+//           await mongodb.dbConnect()
+//           const user = await UserModel.findOne({ username });
+
+//           if (!user) {
+//             return null;
+//           }
+
+//           const passwordsMatch = await bcrypt.compare(passwort, user.passwort);
+
+//           if (!passwordsMatch) {
+//             return null;
+//           }
+
+//           return user;
+//         } catch (error) {
+//           console.log("Error: ", error);
+//         }
+//       },
+//     }),
+//   ],
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+//   pages: {
+//     signIn: "/create",
+//   },
+// };
+
+// const handler = NextAuth(authOptions);
+
+//  export { handler as GET, handler as POST };
--- pages/create/mainMenu.js
+++ pages/create/mainMenu.js
@@ -1,20 +1,21 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 
 import Link from "next/link"
 import styles from '../../styles/Button.module.css'
 import { useState } from "react"
-import cookie from 'cookie'
-import mongodb from "@/utils/mongodb"
-import studie from "@/models/studie"
+
 
 import Popup from '../../komponenten/FirstLoginPopUp'
 
 
 export default function mainMenu() {
 
+    const {data:session} = useSession();
+    console.log({data:session}) 
 
 
     const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -25,11 +26,38 @@ export default function mainMenu() {
       setFormData(data);
     }
     
+
+    if (session) {
+        // Session is still loading or user is not logged in
+        return <div>Session ist da</div>;
+      }
+    
+    //   // Check if user data exists in the session
+    //   if (!session.user) {
+    //     return <div>User ist auch nicht da</div>;
+    //   }
+    
+    //   // Now you can safely access session.user
+    //   const { username, email } = session.user;
+    
+    //   return (
+    //     <div>
+    //       <h1>User Profile</h1>
+    //       <p>Username: {username}</p>
+    //       <p>Email: {email}</p>
+    //     </div>
+    //   );
+
+    // const test1 = () => {
+    //     sessionStorage.setItem("Test", "1");
+    // }
+    
     
   return (
 
      <div className='text-container'>
         <h2>Willkommen zum Erstellen <br/> einer neuen Studie</h2>
+        <p className="text">{session?.user?.username}Hier sollte eigentlich nen Name stehen </p>
         
         <div className='hr-container'>
             <p className='hr-vert'></p>
@@ -63,10 +91,8 @@ export default function mainMenu() {
                 <p className="hr-vert-small"></p>
               </div>	
             <button onClick={() => signOut()}>Logout</button>  
-            {/* <Link href ="/create">
-                <span className="menu-item">Logout</span>
-            </Link> */}
 
+            {/* <button onClick={test1}> hier </button> */}
 
 
             <div>
@@ -92,3 +118,34 @@ export default function mainMenu() {
    
   )
 }
+
+
+
+// "use client";
+
+// import { signOut } from "next-auth/react";
+// import { useSession } from "next-auth/react";
+
+// export default function UserInfo() {
+//   const { data: local } = useSession();
+//   console.log({data: local})
+
+//   return (
+//     <div className="grid place-items-center h-screen">
+//       <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
+//         <div>
+//           Name: <span className="font-bold">{local?.user?.username}</span>
+//         </div>
+//         <div>
+//           Email: <span className="font-bold">{local?.user?.mail}</span>
+//         </div>
+//         <button
+//           onClick={() => signOut()}
+//           className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
+//         >
+//           Log Out
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
