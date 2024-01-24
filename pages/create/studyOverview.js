ad5aea4ad804799728ccb7e25d48bf5547a7f5e9
felixhoentzsch <felix.hoentzsch@icloud.com>
2024-01-24 16:53:23 +0100

    feat: new page with studyOverview, new text across the app

--- pages/create/studyOverview.js
+++ pages/create/studyOverview.js
@@ -6,60 +6,39 @@ import { useRouter } from 'next/router';
 
 import { useState } from "react";
 import Spacer from '@/komponenten/Spacer';
-import MainMenu from './mainMenu';
-
 
 export default function StudyOverview() {
-
     const [success, setSuccess] = useState('Studie erfolgreich angelegt');
-
     const router = useRouter();
     const { studyName, randomize, RandomizationParameter, inputFields, nameFields, blocks } = router.query;
-
-    var method 
-    var value
-    var RandVerh
-    var groupName
-
-
-    if(randomize === "coin"){
-        method = "Biased Coin"
-        value = parseFloat(RandomizationParameter);
-        const remainingString = RandomizationParameter.slice(String(value).length);
-
-        //console.log("Float-Wert:", value);
-        //console.log("Verbleibender String:", remainingString);
-
-    }else if(randomize === "block"){
-        method = "Blockrandomisierung"
-        value = parseInt(RandomizationParameter, 10); // 10 ist die Basis (dezimal)
-        const remainingString = RandomizationParameter.slice(String(value).length);
-
-        const originalString = inputFields.toString(); // In eine Zeichenkette umwandeln
-        RandVerh = originalString.slice(0, inputFields.length - 1) + ':' + originalString.slice(-1);
-
-        //console.log("Ganzzahl-Wert:", value);
-        //console.log("Verbleibender String:", remainingString);
-        //console.log(RandVerh);
-
-    }else if(randomize === "inbalance"){
-        method = "Maximally-Tolerated-Imbalance"
-        value = parseInt(RandomizationParameter, 10); // 10 ist die Basis (dezimal)
-        const remainingString = RandomizationParameter.slice(String(value).length);
-
-        //console.log("Ganzzahl-Wert:", value);
-        //console.log("Verbleibender String:", remainingString);
-
+  
+    // Initialisierung der Variablen
+    let method = "";
+    let value = "";
+    let RandVerh = "";
+    let groupName = "";
+  
+    if (randomize === "coin") {
+      method = "Biased Coin";
+      value = parseFloat(RandomizationParameter);
+    } else if (randomize === "block") {
+      method = "Blockrandomisierung";
+      value = parseInt(RandomizationParameter, 10);
+      const originalString = inputFields.toString();
+      RandVerh = originalString.slice(0, inputFields.length - 1) + ':' + originalString.slice(-1);
+    } else if (randomize === "inbalance") {
+      method = "Maximally-Tolerated-Imbalance";
+      value = parseInt(RandomizationParameter, 10);
     }
-
-    if(nameFields !== ""){
-        const originalObject = nameFields.toString(); // In eine Zeichenkette umwandeln
-        groupName = originalObject.slice(0, nameFields.length - 1) + ':' + originalObject.slice(-1);
+  
+    if (nameFields !== "") {
+      const originalObject = nameFields.toString();
+      groupName = originalObject.slice(0, nameFields.length - 1) + ':' + originalObject.slice(-1);
     }
-
+  
     const handleNavigation = () => {
-        router.push('/')
-    }    
+      router.push('/');
+    };
     
   return (
     <div className='text-container'>
